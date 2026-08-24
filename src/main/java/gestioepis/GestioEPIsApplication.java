f3b0052e0505b92bf2e4618fbbc96bf04608d5b8
sergibarios <bariosbergua@gmail.com>
2026-08-24 13:11:42 +0200

    Initial commit

---
+++ src/main/java/gestioepis/GestioEPIsApplication.java
@@ -0,0 +1,13 @@
+package gestioepis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioEPIsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(GestioEPIsApplication.class, args);
+    }
+
+}
--- src/test/java/gestioepis/GestioEPIsApplicationTests.java
+++ src/test/java/gestioepis/GestioEPIsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.gestioepis;
+package gestioepis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestioEpIsApplicationTests {
+class GestioEPIsApplicationTests {
 
     @Test
     void contextLoads() {
