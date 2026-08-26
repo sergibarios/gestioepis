951cd4a26846f4bc40eb44afc49f1d02d72781ae
otto616 <ottobielrodenas@gmail.com>
2026-08-26 11:14:20 +0200

    purchase orders page implemented

--- src/main/java/gestioepis/models/PurchaseOrder.java
+++ src/main/java/gestioepis/models/PurchaseOrder.java
@@ -1,13 +1,17 @@
 package gestioepis.models;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Table(name = "purchase_order")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PurchaseOrder {
